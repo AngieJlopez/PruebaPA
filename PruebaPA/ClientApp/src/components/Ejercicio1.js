70a047bac7e35297bf4c01a2fc64cb22eb00a0e4
Angie Jaramillo <angie.jaramillo@inacapmail.cl>
2022-07-21 22:49:10 -0400

    Agregar archivos de proyecto.

---
+++ PruebaPA/ClientApp/src/components/Ejercicio1.js
@@ -0,0 +1,38 @@
+﻿import React, { useState, useEffect } from 'react'
+
+
+const Ejercicio1 = () => {
+    return (
+        <>
+            <div className="card mb-3">
+                <img src="https://as01.epimg.net/meristation/imagenes/2020/03/07/reportajes/1583579604_812509_1583579788_sumario_normal.jpg" class="card-img-top" alt="30"/>
+                    <div className="card-body">
+                        <h5 className="card-title">Angie Jaramillo López</h5>
+                    <p className="card-text">Me considero una persona comprometida, responsable y con amplia capacidad para adquirir nuevos conocimientos.</p>
+                        
+                    </div>
+</div>
+                <div className="card">
+                    <div className="card-body">
+                    <h5 className="card-title">Angie Jaramillo López</h5>
+                    <p className="card-text">Me considero una persona alegre, comprometida, responsable y con amplia capacidad para adquirir nuevos conocimientos.</p>
+                        
+                    </div>
+                <img src="https://static.abc.es/Media/201503/05/kitana-mortal-kombat-x--644x362.jpg" class="card-img-bottom" alt="30" />
+</div>
+            <h1 className="mt-4">Habilidades </h1>
+            <ol class="list-group list-group-numbered">
+                <li class="list-group-item">Adaptabilidad</li>
+                <li class="list-group-item">Creatividad</li>
+                <li class="list-group-item">Productividad</li>
+                <li class="list-group-item">Responsabilidad</li>
+                <li class="list-group-item">Liderazgo</li>
+                <li class="list-group-item">Cordialidad</li>
+            </ol>
+
+        </>
+    )
+}
+export default Ejercicio1
+
+
---
+++ PruebaPA/ClientApp/src/components/Ejercicio2.js
@@ -0,0 +1,166 @@
+﻿import React, { useState, useEffect } from 'react'
+
+const UsuarioPorDefecto = [
+    { nombres: "Patana", mensaje: "Triviño", Like: true },
+    { nombres: "Juan Carlos", mensaje: "Bodoque", Like: false }
+]
+const Ejercicio2 = () => {
+
+
+    const [individuo, setindividuo] = useState(UsuarioPorDefecto)
+    const [nombres, setNombres] = useState("")
+    const [mensaje, setMensaje] = useState("")
+    const [Like, setLike] = useState(false)
+    const [Saludo, setSaludo] = useState(false)
+
+    useEffect(() => {
+        let copiaindividuo = [...individuo]
+        const nuevoindividuo = {
+            nombres: "Juanin Juan",
+            mensaje: "Harry",
+            Like: true,
+            Saludo: true
+        }
+        copiaindividuo.push(nuevoindividuo)
+        setindividuo(copiaindividuo)
+    }, [])
+
+    const handleNombres = (evento) => {
+        setNombres(evento.target.value)
+    }
+
+    const handleMensaje = (evento) => {
+        setMensaje(evento.target.value)
+    }
+
+    const handleLike = (evento) => {
+        setLike(!Like)
+    }
+
+    const handleSaludo = (evento) => {
+        setSaludo(!Saludo)
+    }
+
+    const handleIngresoindividuo = (evento) => {
+        console.log("Botón presionado")
+        const nuevoindividuo = {
+            nombres: nombres,
+            mensaje: mensaje,
+            Like: Like,
+            Saludo: Saludo
+        }
+
+        let copiaindividuo = [...individuo]
+        copiaindividuo.push(nuevoindividuo)
+        setindividuo(copiaindividuo)
+    }
+    return (
+        <>
+            <div className="row">
+                <div className="col-12 text-center">
+                    <h1>Enviar Mensaje</h1>
+                </div>
+            </div>
+
+            <div className="col-4">
+                <label htmlFor="nombres">Nombres</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    onChange={handleNombres}
+                    value={nombres}
+                    name="nombres"
+                    id="nombres"
+                />
+            </div>
+
+            <div className="col-4">
+                <label htmlFor="mensaje">Mensaje</label>
+                <textarea
+                    className="form-control"
+                    type="text"
+                    onChange={handleMensaje}
+                    value={mensaje}
+                    name="mensaje"
+                    id="mensaje"
+                />
+            </div>
+            <div className="col-4">
+                <br /> { }
+                <div className="form-check form-check-inline">
+                    <input
+                        className="form-check-input"
+                        type="checkbox"
+                        checked={Like}
+                        onChange={handleLike}
+                        name="Like"
+                        id="Like" />
+                    <label
+                        className="form-check-label"
+                        htmlFor="Like">
+                        👍Dar Like
+                    </label>
+                </div>
+                <div className="form-check form-check-inline">
+
+                    <input
+                        className="form-check-input"
+                        type="checkbox"
+                        checked={Saludo}
+                        onChange={handleSaludo}
+                        name="Saludo"
+                        id="Saludo"/>
+                    <label
+                        className="form-check-label"
+                        htmlFor="Saludo">
+                        👋​Enviar Saludo
+                    </label>
+
+                </div>
+
+            </div>
+            <div className="col-12 justify-content-start mt-3">
+                <button
+                    className="btn btn-primary"
+                    type="button"
+                    onClick={handleIngresoindividuo}
+                >
+                    Enviar
+                </button>
+            </div>
+
+            <hr />
+            <div className="row mt-5">
+                <div className="col-12">
+                    <table className="table table-stripped">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Reacciones</th>
+                                <th>Mensaje</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {individuo.map((individuo) => {
+                                return (
+                                    <tr>
+                                        <td>{individuo.nombres}</td>
+
+                                        <td>{individuo.Like ?
+                                            " 👍 " :
+                                            "<Ninguno>"
+                                        }
+                                        </td>
+                                        <td>{individuo.mensaje}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Ejercicio2
